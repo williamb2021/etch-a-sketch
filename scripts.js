9b32edb0ba30d845e0bef25a6bbd9fc3ecc2bf92
williamb2021 <bonewj@gmail.com>
2022-01-02 21:46:05 -0800

    Added prompt to reset button to set grid size, cleaned things up

--- scripts.js
+++ scripts.js
@@ -1,7 +1,14 @@
 const container = document.querySelector(".wrapper");
 const resetButton = document.querySelector(".reset");
-resetButton.addEventListener('click',() => {
-    clearGrid();
+
+resetButton.addEventListener('click', () => {
+        let newGridSize = parseInt(prompt("Please enter a new grid size (max 100)!"));
+        if (newGridSize > 100 || isNaN(newGridSize) ){
+            alert("You must enter a number 100 or smaller, please try again");
+        }
+        else {
+            clearGrid(newGridSize);
+        }
 });
 
 function loadGrid(size){
@@ -9,24 +16,24 @@ function loadGrid(size){
     let gridTotal = size*size;
     for (let i=0; i < gridTotal; i++){
         const grid = document.createElement("div");
-        grid.classList.add("box");
-        // grid.style.cssText = `flex-basis: ${flexPercentage}%`;    
+        grid.classList.add("box"); 
         container.appendChild(grid);
     }
 
     const boxes = document.querySelectorAll(".box");
     boxes.forEach((box) => {  
-    box.addEventListener('mouseenter',(e) => {
-        e.target.style.background = "black";
+        box.addEventListener('mouseenter',(e) => {
+            e.target.style.background = "black";
+        });
     });
-});
 }
 
-function clearGrid(){
+function clearGrid(size){
     const boxes = document.querySelectorAll(".box");
     boxes.forEach((box) => {  
         box.remove();
     });
+    loadGrid(size);
 }
 
 //default is 16x16 grid
